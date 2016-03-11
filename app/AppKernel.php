36cec69f0c9374e231f0fb3c478f7d692b556226
Khalid Sookia <khalidsookia@gmail.com>
2016-03-11 12:48:10 +0100

    Installed and configured fos user

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new intro\CrudBundle\introCrudBundle(),
             new Blog\AppBundle\BlogAppBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
