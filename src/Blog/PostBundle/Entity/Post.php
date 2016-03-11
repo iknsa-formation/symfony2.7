412a80b617d9cc76baeb3cd99c226cc463c4268e
Khalid Sookia <khalidsookia@gmail.com>
2016-03-11 22:44:04 +0100

    typo correction

--- src/Blog/PostBundle/Entity/Post.php
+++ src/Blog/PostBundle/Entity/Post.php
@@ -18,32 +18,32 @@ class Post
     protected $id;
 
     /**
-     * @ORM\Column(name="title" type="string", length=255)
+     * @ORM\Column(name="title", type="string", length=255)
      */
     protected $title;
 
     /**
-     * @ORM\Column(name="summary" type="string", length=255)
+     * @ORM\Column(name="summary", type="string", length=255)
      */
     protected $summary;
 
     /**
-     * @ORM\Column(name="content" type="text")
+     * @ORM\Column(name="content", type="text")
      */
     protected $content;
 
     /**
-     * @ORM\Column(name="created_at" type="datetime")
+     * @ORM\Column(name="created_at", type="datetime")
      */
     protected $created_at;
 
     /**
-     * @ORM\Column(name="image" type="string", length=255)
+     * @ORM\Column(name="image", type="string", length=255)
      */
     protected $image;
 
     /**
-     * @ORM\Column(name="user" type="string", length=255)
+     * @ORM\Column(name="user", type="string", length=255)
      */
     protected $user;
 }
