fe0e2a1cb533a03ff3a559705de294b9bcf41078
Khalid Sookia <khalidsookia@gmail.com>
2016-03-12 13:56:22 +0100

    Handling new post form submission with database persistance and

--- src/Blog/PostBundle/Controller/PostController.php
+++ src/Blog/PostBundle/Controller/PostController.php
@@ -4,6 +4,7 @@ namespace Blog\PostBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Blog\PostBundle\Entity\Post;
+use Symfony\Component\HttpFoundation\Request;
 
 class PostController extends Controller
 {
@@ -12,11 +13,34 @@ class PostController extends Controller
         return $this->render('BlogPostBundle:post:index.html.twig');
     }
 
-    public function newAction()
+    public function createAction(Request $request)
     {
         $entity = new Post;
+
+        $form = $this->createCreateForm($entity);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('blog_post_new'));
+        }
+
+        return $this->render('BlogPostBundle:post:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    public function createCreateForm($entity)
+    {
         $form = $this->createFormBuilder($entity)
 
+            ->setAction($this->generateUrl('blog_post_create'))
+            ->setMethod('POST')
             ->add('title')
             ->add('summary')
             ->add('content')
@@ -25,11 +49,19 @@ class PostController extends Controller
             ->add('user')
             ->add('submit', 'submit', array('label' => 'Create'))
 
-            ->getForm()
-            ->createView();
+            ->getForm();
+
+            return $form;
+    }
+
+    public function newAction()
+    {
+        $entity = new Post;
+
+        $form = $this->createCreateForm($entity);
 
         return $this->render('BlogPostBundle:post:new.html.twig', array(
-            'form'   => $form,
+            'form'   => $form->createView(),
         ));
     }
 }
