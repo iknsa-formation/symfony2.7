cdf05fa9cb5d8fc00758a18a8b4e26f9f75cdd62
Khalid Sookia <khalidsookia@gmail.com>
2016-03-12 12:08:01 +0100

    Showing form from new action

--- src/Blog/PostBundle/Controller/PostController.php
+++ src/Blog/PostBundle/Controller/PostController.php
@@ -3,11 +3,34 @@
 namespace Blog\PostBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Blog\PostBundle\Entity\Post;
 
 class PostController extends Controller
 {
     public function indexAction()
     {
         return $this->render('BlogPostBundle:post:index.html.twig');
+    }
+
+    public function newAction()
+    {
+        $entity = new Post;
+
+        $form = $this->createFormBuilder($entity)
+
+            ->add('title')
+            ->add('summary')
+            ->add('content')
+            ->add('image')
+            ->add('created_at')
+            ->add('user')
+            ->add('submit', 'submit', array('label' => 'Create'))
+
+            ->getForm();
+
+        return $this->render('BlogPostBundle:post:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
     }
 }
